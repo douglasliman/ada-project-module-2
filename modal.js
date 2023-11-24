1f78bfcfbbf71c091cde44d3a75a7d6b468c9727
Douglas Lima <douglas.lima.queiroz@gmail.com>
2023-11-24 19:36:30 -0300

    fix -md

--- modal.js
+++ modal.js
@@ -1,24 +1,42 @@
+document.addEventListener('click', function (event) {
+    const target = event.target;
+
+    const btnUpdate = target.closest('.update');
+    if (btnUpdate) {
+        const taskId = btnUpdate.getAttribute('data-task-id');
+        updateTask(taskId);
+    }
+});
+const btnUpdate = document.querySelector('.update');
 const modal = document.querySelector('.modal');
 const modalInput = document.getElementById('modal-input');
 const inputUUID = document.getElementById('UUID');
-const btnUpdate = document.querySelector('update');
 const btnCancel = document.getElementById('modal-cancel-btn');
 const btnSave = document.getElementById('modal-save-btn');
 
-function updateTask(elemento) {
-	modal.style.display = 'block';
+function updateTask(taskId) {
+    modal.style.display = 'block';
 
-	const UUID = elemento.getAttribute('data-task-id');
-	modalInput.value = document.getElementById(UUID).getElementsByTagName('div')[0].innerHTML;
-	inputUUID.value = UUID;
+    modalInput.value = document.getElementById(taskId).getElementsByTagName('div')[0].innerHTML;
+    inputUUID.value = taskId;
 }
 
 btnSave.addEventListener('click', () => {
-	document.getElementById(inputUUID.value).getElementsByTagName('div')[0].innerHTML =
-		modalInput.value;
-	modal.style.display = 'none';
+    document.getElementById(inputUUID.value).getElementsByTagName('div')[0].innerHTML =
+        modalInput.value;
+    modal.style.display = 'none';
 });
 
 btnCancel.addEventListener('click', () => {
-	modal.style.display = 'none';
+    modal.style.display = 'none';
+});
+
+document.addEventListener('click', function (event) {
+    const target = event.target;
+
+    const btnUpdate = target.closest('.update');
+    if (btnUpdate) {
+        const taskId = btnUpdate.getAttribute('data-task-id');
+        updateTask(taskId);
+    }
 });
